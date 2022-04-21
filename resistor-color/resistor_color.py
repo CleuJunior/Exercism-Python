ef34a23821089a1cbd4071675dc9ed67cbdc85a1
Cleonildo Jr <cleu.junior@gmail.com>
2022-04-21 13:27:46 -0300

    Resistor Color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+COLOR_VALUES = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+]
+
+
+def color_code(color):
+    return COLOR_VALUES.index(color)
+
+
+def colors():
+    return COLOR_VALUES
